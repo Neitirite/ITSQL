6342dbbfcb94796ba545777f9692b5e0f5052358
Neitirite <belohvostiyorlan@gmail.com>
2025-05-30 22:43:45 +0300

    Initial commit

---
+++ src/main/kotlin/com/Neitirite/UserDBController.kt
@@ -0,0 +1,58 @@
+package com.Neitirite
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import java.sql.Connection
+import java.sql.DriverManager
+import java.util.*
+import kotlinx.coroutines.*
+@Serializable
+data class userInfo(val username: String, val Group: String, val password: String)
+
+val db_url = "jdbc:postgresql://192.168.0.13:5432/authorization"
+val db_username = "SYSDBA"
+val db_password = "masterkey"
+val props = mapOf("user" to db_username, "password" to db_password)
+
+
+class UserDBController {
+    fun Register(data: String) {
+        val parsedData = Json.decodeFromString<userInfo>(data)
+        val surname = parsedData.username.split(" ")[0]
+        val name = parsedData.username.split(" ")[1]
+        val group = parsedData.Group
+        val password = parsedData.password
+        println("parsed data is ${surname}, ${name}, ${group}, ${password}")
+
+        val conn = DriverManager.getConnection(db_url, props.toProperties())
+        val st = conn.createStatement()
+
+        val getUsers = st.executeQuery("SELECT * FROM users WHERE surname = '${surname}' AND name = ${name} AND groupname = '${group}'")
+        if(getUsers.next()) {
+            println("пользователь уже существует")
+        } else {
+            val rs = st.executeQuery(
+                "insert into users (surname, name, groupname, password) values ('${surname}', '${name}', '${group}', '${password}')")
+            rs.close()
+        }
+        getUsers.close()
+        st.close()
+    }
+
+    fun SignIn(data: String) {
+        val parsedData = Json.decodeFromString<userInfo>(data)
+        val surname = parsedData.username.split(" ")[0]
+        val name = parsedData.username.split(" ")[1]
+        val group = parsedData.Group
+        val password = parsedData.password
+
+        val conn = DriverManager.getConnection(db_url, props.toProperties())
+        val st = conn.createStatement()
+        val getUsers = st.executeQuery("SELECT * FROM users WHERE surname = '${surname}' AND name = ${name} AND groupname = '${group}'")
+        if(getUsers.next()) {
+            println("Пользователь найден")
+            TODO("Надо сделать ответ на фронт")
+        } else {
+            println("Пользователь не существует")
+        }
+    }
+}
