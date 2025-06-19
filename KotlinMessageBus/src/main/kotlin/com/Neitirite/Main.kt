e92b0fa47a976671a703313d187b8f3c3dda167f
Neitirite <belohvostiyorlan@gmail.com>
2025-06-19 21:39:51 +0300

    Added the ability to create  nd delete topics, as well as send messages

--- KotlinMessageBus/src/main/kotlin/com/Neitirite/Main.kt
+++ KotlinMessageBus/src/main/kotlin/com/Neitirite/Main.kt
@@ -6,11 +6,8 @@ import io.ktor.server.plugins.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.*
 import kotlin.time.Duration.Companion.seconds
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.jsonPrimitive
-
 
 fun main() {
     embeddedServer(Netty, 5000, "0.0.0.0") {
@@ -43,12 +40,17 @@ fun main() {
                                     }
                                     "sendMessage" -> {
                                         println(command.properties["topic"]?.jsonPrimitive?.content)
-                                        println(command.properties["message"]?.jsonPrimitive?.content)
+                                        println(command.properties["message"])
+                                        val topic = command.properties["topic"]?.jsonPrimitive?.content
+                                        val response = Topics().sendMessage(topic.toString(),
+                                                command.properties["message"] as JsonObject)
+                                        send(response)
                                     }
                                     "deleteTopic" -> {
-                                        val topic = command.properties["topic"]?.jsonPrimitive?.content
+                                        val topic = command.properties["name"]?.jsonPrimitive?.content
                                         try {
-                                            Topics().deleteTopic(topic.toString())
+                                            val response = Topics().deleteTopic(topic.toString())
+                                            send(response)
                                         } catch (e: Exception) {
                                             println("Failed to delete topic: ${e.message}")
                                         }
--- KotlinMessageBus/src/main/kotlin/com/Neitirite/Topics.kt
+++ KotlinMessageBus/src/main/kotlin/com/Neitirite/Topics.kt
@@ -1,9 +1,11 @@
 package com.Neitirite
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.encodeToJsonElement
 import java.lang.System
 import java.io.File
 import java.nio.file.Files.deleteIfExists
 
-var isTopicFileBusy = false
 val topicDirectory = File(System.getenv("TOPICS_PATH").toString())
 class Topics {
     fun createTopic(topicName: String): String {
@@ -18,7 +20,7 @@ class Topics {
 
     }
 
-    fun deleteTopic(topicName: String): String{
+    fun deleteTopic(topicName: String?): String{
         val topicFile = File("${topicDirectory}/$topicName")
         deleteIfExists(topicFile.toPath())
         if(topicFile.exists()){
@@ -29,19 +31,35 @@ class Topics {
         }
     }
 
-    fun sendMessage(topicName: String, message: String): String{
+    fun sendMessage(topicName: String, message: JsonObject): String{
+        println(topicName)
         val topicFile = File("${topicDirectory}/$topicName")
         if(!topicFile.exists()){
             println("Failed to send message in $topicName. File does not exist: ${topicFile.absolutePath}")
             return "Failed to send message in $topicName: Topic does not exist"
         } else {
-            if(!isTopicFileBusy){
-                isTopicFileBusy = true
-
-            } else {
-
-            }
+            fetchMessages(topicName, message)
             return "Successfully sent message $message in $topicName"
         }
     }
+
+    fun fetchMessages(topic: String, message: JsonObject){
+        val topicFile = File("${topicDirectory}/$topic")
+        val messages = topicFile.readText()
+        println(messages)
+        if(messages != ""){
+            var parsedMessages = Json.decodeFromString<List<Serialization.Messages>>(messages)
+            val newMessage = Serialization.Messages((parsedMessages.last().id.toInt() + 1).toString(), message)
+            println("New messages: $newMessage")
+            parsedMessages += newMessage
+            println(Json.encodeToJsonElement(parsedMessages))
+            topicFile.writeText(Json.encodeToJsonElement(parsedMessages).toString())
+        } else {
+            val newMessage = Serialization.Messages("0", message)
+            val updatedMessages = Json.encodeToString(newMessage)
+            topicFile.writeText("[$updatedMessages]")
+        }
+
+    }
+
 }
